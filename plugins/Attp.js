ed9b80ae71154402f07b8f2ca39d88f489ea10ec
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-09-01 06:31:21 -0500

    Update Attp.js

--- plugins/Attp.js
+++ plugins/Attp.js
@@ -1,17 +1,46 @@
-import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
-let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-if (!text) throw `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙋𝘼𝙍𝘼 𝙌𝙐𝙀 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊 𝙎𝙀 𝘾𝙊𝙉𝙑𝙄𝙀𝙍𝙏𝘼 𝙀𝙉 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command}* Nuevo Sticker\n\n𝙒𝙍𝙄𝙏𝙀 𝙎𝙊 𝙏𝙃𝙀 𝙏𝙀𝙓𝙏 𝘽𝙀𝘾𝙊𝙈𝙀𝙎 𝘼 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command}* New Sticker*`
-let teks = encodeURI(text)
+import axios from 'axios';
 
-if (command == 'attp') {
-let stiker = await sticker(null,`https://api.erdwpe.com/api/maker/attp?text=${teks}`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })}
+let handler = async (m, { conn, text }) => {
+  await m.reply("Buscando...");
+  if (!text) return conn.reply(m.chat, "Ingrese una dirección IP válida", m);
 
-if (command == 'ttp') {
-let stiker = await sticker(null,`https://api.erdwpe.com/api/maker/ttp?text=${teks}`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })}
+  try {
+    let res = await axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`);
+    const data = res.data;
+
+    if (data.status !== "success") {
+      return conn.reply(m.chat, data.message || "Falló", m);
+    }
+
+    let ipsearch = ` 
+    𝐈𝐏 𝐈𝐍𝐅𝐎
+
+    IP : ${data.query}
+    País : ${data.country}
+    Código de País : ${data.countryCode}
+    Provincia : ${data.regionName}
+    Código de Provincia : ${data.region}
+    Ciudad : ${data.city}
+    Distrito : ${data.district}
+    Código Postal : ${data.zip}
+    Coordenadas : ${data.lat}, ${data.lon}
+    Zona Horaria : ${data.timezone}
+    ISP : ${data.isp}
+    Organización : ${data.org}
+    AS : ${data.as}
+    Mobile : ${data.mobile ? "Si" : "No"}
+    Hosting : ${data.hosting ? "Si" : "No"}
+    `.trim();
+
+    await conn.reply(m.chat, ipsearch, m);
+  } catch (error) {
+    console.error(error);
+    await conn.reply(m.chat, 'Ocurrió un error al obtener la información de la IP.', m);
+  }
 }
-handler.command = handler.help = ['ttp', 'attp']
-handler.tags = ['sticker']
-export default handler
+
+handler.tags = ['tools'];
+handler.command = /^(ip|ipcheck|ipcek)$/i;
+handler.owner = true;
+
+export default handler;
