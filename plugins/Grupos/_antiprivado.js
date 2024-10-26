3060bd5e0155d5e68fd6f841dec70c259e2d4508
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-10-26 07:59:58 -0500

    Actualizar _antiprivado.js

--- plugins/Grupos/_antiprivado.js
+++ plugins/Grupos/_antiprivado.js
@@ -7,7 +7,7 @@ export async function before(m, {isOwner, isROwner}) {
 
   if (bot.antiPrivate && !isOwner && !isROwner) {
     await m.reply(
-     *sebxs bot es un bot privado y tiene prohibido los mensajes a mi privado | Seras bloquead.o*
+     sebxs bot es un bot privado y tiene prohibido los mensajes a mi privado | seras bloqueado.
  
 Creador:
 👤 +593992402778
