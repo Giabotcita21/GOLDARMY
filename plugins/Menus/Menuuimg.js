c1606ca17e4eaccd1c62142d78a014b1f33d983e
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-11-09 02:12:47 -0500

    Update Menuuimg.js

--- plugins/Menus/Menuuimg.js
+++ plugins/Menus/Menuuimg.js
@@ -59,7 +59,7 @@ let menu = `
 
  `.trim()
     
-const vi = ['https://telegra.ph/file/398b27455d9a29ddfb054.mp4']
+const vi = ['https://imgur.com/TiaL3Ru']
 
 try {
 await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
