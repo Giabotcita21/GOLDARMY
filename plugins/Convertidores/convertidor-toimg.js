c55a317f10ad0e2c24868a2b50455a2c1f12a4de
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-11-11 08:11:15 -0500

    𝑺𝑬𝑩𝑿𝑺 𝑩𝑶𝑻

--- plugins/Convertidores/convertidor-toimg.js
+++ plugins/Convertidores/convertidor-toimg.js
@@ -1,6 +1,6 @@
 import {webp2png} from '../../lib/webp2mp4.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const notStickerMessage = `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙸𝙼𝙰𝙶𝙴𝙽 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
+  const notStickerMessage = `*[❗𝑰𝑵𝑭𝑶❗]  𝑹𝑬𝑺𝑷𝑶𝑵𝑫𝑨 𝑨𝑳 𝑺𝑻𝑰𝑪𝑲𝑬𝑹 𝑸𝑼𝑬 𝑫𝑬𝑺𝑬𝑬 𝑪𝑶𝑵𝑽𝑬𝑹𝑻𝑰𝑹 𝑬𝑵 𝑰𝑴𝑨𝑮𝑬𝑵 𝑪𝑶𝑵 𝑬𝑳 𝑪𝑶𝑴𝑨𝑵𝑫𝑶. ${usedPrefix + command}*`;
   if (!m.quoted) throw notStickerMessage;
   const q = m.quoted || m;
   const mime = q.mediaType || '';
