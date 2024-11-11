be2af5eb7da4eff906a47d633afaf300d76e8e03
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-11-11 08:09:34 -0500

    𝑺𝑬𝑩𝑿𝑺 𝑩𝑶𝑻

--- plugins/Convertidores/convertidor-togif.js
+++ plugins/Convertidores/convertidor-togif.js
@@ -1,15 +1,15 @@
 /* 𝐂𝐑𝐄𝐀𝐃𝐎 𝐏𝐎𝐑 https://github.com/BrunoSobrino */
 
 let handler = async (m, {conn}) => {
-  if (!m.quoted) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙶𝙸𝙵 𝙲𝙾𝙽 𝙰𝚄𝙳𝙸𝙾*`;
+  if (!m.quoted) throw `*[❗𝑰𝑵𝑭𝑶 ❗] 𝑹𝑬𝑺𝑷𝑶𝑵𝑫𝑨 𝑨 𝑼𝑵 𝑨𝑼𝑫𝑰𝑶 𝑸𝑼𝑬 𝑫𝑬𝑺𝑬𝑬 𝑪𝑶𝑵𝑽𝑬𝑹𝑻𝑰𝑹 𝑬𝑵 𝑮𝑰𝑭 𝑪𝑶𝑵 𝑨𝑼𝑫𝑰𝑶.*`;
   const q = m.quoted || m;
   let mime = (q.msg || q).mimetype || "";
-  if (!/(mp4)/.test(mime)) throw `*[❗] 𝙴𝙻 𝚃𝙸𝙿𝙾 𝙳𝙴 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 ${mime} 𝙽𝙾 𝙴𝚂 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾, 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙶𝙸𝙵 𝙲𝙾𝙽 𝙰𝚄𝙳𝙸𝙾*`;
+  if (!/(mp4)/.test(mime)) throw `*[❗] 𝑬𝑳 𝑻𝑰𝑷𝑶 𝑫𝑬 𝑨𝑹𝑪𝑯𝑰𝑽𝑶 ${mime} 𝑵𝑶 𝑬𝑺 𝑪𝑶𝑹𝑹𝑬𝑪𝑻𝑶, 𝑹𝑬𝑺𝑷𝑶𝑵𝑫𝑨 𝑨 𝑼𝑵 𝑽𝑰𝑫𝑬𝑶 𝑸𝑼𝑬 𝑫𝑬𝑺𝑬𝑬 𝑪𝑶𝑵𝑽𝑬𝑹𝑻𝑰𝑹 𝑬𝑵 𝑮𝑰𝑭 𝑪𝑶𝑵 𝑨𝑼𝑫𝑰𝑶.*`;
   m.reply(global.wait);
   let media = await q.download();
   conn.sendMessage(
     m.chat,
-    {video: media, gifPlayback: true, caption: "*ᴀϙᴜɪ ᴇsᴛᴀ sᴜ ɢɪғ ᴄᴏɴ ᴀᴜᴅɪᴏ, ᴀʟ ᴀʙʀɪʀʟᴏ sᴇ ʀᴇᴘʀᴏᴅᴜᴄᴇ ᴄᴏɴ ᴀᴜᴅɪᴏ*"},
+    {video: media, gifPlayback: true, caption: "*𝑨𝑸𝑼𝑰 𝑬𝑺𝑻𝑨 𝑺𝑼 𝑮𝑰𝑭 𝑪𝑶𝑵 𝑨𝑼𝑫𝑰𝑶, 𝑨𝑳 𝑴𝑶𝑴𝑬𝑵𝑻𝑶 𝑫𝑬 𝑨𝑩𝑹𝑰𝑹𝑳𝑶 𝑺𝑬 𝑹𝑬𝑷𝑹𝑶𝑫𝑼𝑪𝑬 𝑪𝑶𝑵 𝑨𝑼𝑫𝑰𝑶.*"},
     {quoted: m}
   );
 };
