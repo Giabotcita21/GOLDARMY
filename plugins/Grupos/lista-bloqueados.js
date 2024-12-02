22f648f9423d0ac0407d0c280dc7bbb5d5fb098b
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-12-01 22:24:16 -0500

    Actualizar lista-bloqueados.js

--- plugins/Grupos/lista-bloqueados.js
+++ plugins/Grupos/lista-bloqueados.js
@@ -2,7 +2,7 @@
 let handler = async (m, { conn }) => {
 	
 	await conn.fetchBlocklist().then(async data => {
-		let txt = `⛔ 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗢 𝗗𝗘 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦 𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢𝗦.\n\n𝗠𝗼𝘁𝗶𝘃𝗼:\n𝖲𝗉𝖺𝗆, 𝗅𝗂𝗇𝗄𝗌 𝗒 𝗅𝗅𝖺𝗆𝖺𝖽𝖺𝗌 𝗁𝖺𝖼𝗂𝖺 𝖤𝗅𝗂𝗍𝖾𝖡𝗈𝗍𝖦𝗅𝗈𝖻𝖺𝗅.\n\n𝗧𝗼𝘁𝗮𝗹 :\n ${data.length}\n\n╭━━━ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦 👤\n`
+		let txt = `⛔ 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗢 𝗗𝗘 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦 𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢𝗦.\n\n𝗠𝗼𝘁𝗶𝘃𝗼:\n𝖲𝗉𝖺𝗆, 𝗅𝗂𝗇𝗄𝗌 𝗒 𝗅𝗅𝖺𝗆𝖺𝖽𝖺𝗌 𝗁𝖺𝖼𝗂𝖺 SebxsBot.\n\n𝗧𝗼𝘁𝗮𝗹 :\n ${data.length}\n\n╭━━━ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦 👤\n`
 		for (let i of data) {
 			txt += `┃🚫 @${i.split("@")[0]}\n`
 		}
