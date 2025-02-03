57acf870f445f1c88847edc4b0167d0aabd004e8
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2025-02-03 14:12:16 -0500

    Actualizar info-creadora.js

--- plugins/Propietario/info-creadora.js
+++ plugins/Propietario/info-creadora.js
@@ -2,7 +2,7 @@ var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = 'https://wallpapers.com/images/featured/scream-mz64o06bjspf0jnu.jpg'
 const cat = `𝗖𝗥𝗘𝗔𝗗𝗢𝗥 𝗗𝗘 𝗦𝗘𝗕𝗫𝗦 𝗕𝗢𝗧
-🥷🏼 https://Wa.me/593992402778
+🥷🏼 https://Wa.me/593990990112
 
 
 ✨ 𝗥𝗘𝗗𝗘 𝗦𝗢𝗖𝗜𝗔𝗟 𝗖𝗥𝗘𝗔𝗗𝗢𝗥 
